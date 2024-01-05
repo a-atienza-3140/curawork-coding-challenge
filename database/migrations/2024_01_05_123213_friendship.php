15cc2189722164bd24088302fd8dcb349004a076
Wutz <johneleazar.perez@gmail.com>
2024-01-05 22:54:26 +0800

    fix migration

--- database/migrations/2024_01_05_123213_friendship.php
+++ database/migrations/2024_01_05_123213_friendship.php
@@ -17,11 +17,13 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('friend_id');
-            $table->enum('status', ['pending', 'accepted', 'blocked'])->default('pending');
+            $table->unsignedBigInteger('sender_id');
+            $table->enum('status', ['pending', 'accepted'])->default('pending');
             $table->timestamps();
         
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('friend_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
         
             $table->unique(['user_id', 'friend_id']);
         });
