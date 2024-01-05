7859c5c90aa512410b4ef7ac3031088af8505dd7
Wutz <johneleazar.perez@gmail.com>
2024-01-05 22:48:36 +0800

    code clean up

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,11 @@ use App\Http\Controllers\Api\UsersController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
     Route::get('/friends', [FriendsController::class, 'index']);
     Route::delete('/friends/remove', [FriendsController::class, 'destroy']);
 
